63e54f4c958c1f4eee5de47ff3fbb48d5619ca83
Kalysh <503292@ukr.net>
2019-10-21 16:05:21 +0300

    ADD _ FIX _ input enter item-num?

--- src/components/Reader/Reader.js
+++ src/components/Reader/Reader.js
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
 import React, { Component } from 'react';
+import queryString from 'query-string';
 import Counter from './Counter/Counter';
 import Publication from './Publication/Publication';
 import publications from '../../publications.json';
@@ -7,22 +8,40 @@ import publications from '../../publications.json';
 import css from './Reader.module.css';
 
 class Reader extends Component {
-  state = { value: 0 };
+  state = { value: 1 };
 
-  componentDidUpdate(prevProps, prevState) {
+  componentDidMount() {
     const { history, location } = this.props;
     const { value } = this.state;
 
+    const parsed = Number(queryString.parse(location.search).item);
+
+    if (parsed > publications.length || isNaN(parsed)) {
+      history.push({
+        path: '/reader',
+        search: `?item=1`,
+      });
+    } else if (parsed !== value) {
+      this.setState({
+        value: parsed,
+      });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { history } = this.props;
+    const { value } = this.state;
     if (prevState.value !== value) {
       history.push({
-        ...location,
-        search: `?item=${value + 1}`,
+        path: '/reader',
+        search: `?item=${value}`,
       });
     }
   }
 
   handleIncrement = () => {
-    if (this.state.value === publications.length - 1) {
+    const { value } = this.state;
+    if (value === publications.length) {
       return;
     }
     this.setState(state => ({
@@ -31,7 +50,8 @@ class Reader extends Component {
   };
 
   handleDecrement = () => {
-    if (this.state.value === 0) {
+    const { value } = this.state;
+    if (value === 1) {
       return;
     }
     this.setState(state => ({
@@ -44,12 +64,12 @@ class Reader extends Component {
     return (
       <div className={css.reader}>
         <Counter
-          value={value}
+          value={value - 1}
           length={publications.length}
           handleIncrement={this.handleIncrement}
           handleDecrement={this.handleDecrement}
         />
-        <Publication items={publications[value]} />
+        <Publication items={publications[value - 1]} />
       </div>
     );
   }
